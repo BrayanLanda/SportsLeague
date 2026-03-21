dbcc2b0ad050810a9ea33082f02871f680589537
Brayan Landa <landakkpp@gmail.com>
2026-03-21 12:05:57 -0500

    feat: add team service

--- SportsLeague.Domain/Interfaces/Services/ITeamService.cs
+++ SportsLeague.Domain/Interfaces/Services/ITeamService.cs
@@ -1,6 +1,12 @@
+using SportsLeague.Domain.Entities;
+
 namespace SportsLeague.Domain.Interfaces.Services;
 
 public interface ITeamService
 {
-    
+    Task<IEnumerable<Team>> GetAllAsync();
+    Task<Team?> GetByIdAsync(int id);
+    Task<Team> CreateAsync(Team team);
+    Task<Team> UpdateAsync(Team team);
+    Task<Team> DeleteAsync(int id);
 }
