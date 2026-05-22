4c554f0c521ac3b14b6f4575c51c40a2152ca744
Brayan Landa <landakkpp@gmail.com>
2026-05-21 22:02:58 -0500

    feat: add MatchEventController and DTOs for match results, goals, and cards with mapping configurations

--- SportsLeague.API/Mappings/MappingProfile.cs
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -53,6 +53,22 @@ namespace SportsLeague.API.Mappings
                 .ForMember(dest => dest.RefereeFullName,
                     opt => opt.MapFrom(src =>
                         src.Referee.FirstName + " " + src.Referee.LastName));
+
+            CreateMap<MatchResultRequestDTO, MatchResult>();
+            CreateMap<MatchResult, MatchResultResponseDTO>();
+
+            CreateMap<GoalRequestDTO, Goal>();
+            CreateMap<Goal, GoalResponseDTO>()
+                .ForMember(dest => dest.PlayerName,
+                    opt => opt.MapFrom(src =>
+                        src.Player.FirstName + " " + src.Player.LastName));
+
+            CreateMap<CardRequestDTO, Card>();
+            CreateMap<Card, CardResponseDTO>()
+                .ForMember(dest => dest.PlayerName,
+                    opt => opt.MapFrom(src =>
+                        src.Player.FirstName + " " + src.Player.LastName));
+
         }
     }
 }
